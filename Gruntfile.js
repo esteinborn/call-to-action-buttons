b803cd0e3c9a06291e6b9c635ceacfa098f1f07c
Eric Steinborn <esteinborn@gmail.com>
2013-10-04 14:37:28 -0400

    minified css. longer transitions on old buttons. removed unused css and scss files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,89 @@
 /*global module: false */
 module.exports = function(grunt) {
 
-    // Project configuration
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        watch: {
-            sass: {
-                files: [
-                    'scss/**/*.scss'
-                ],
-                tasks: [
-                    'compass:dev',
-                    'beep:3'
-                ]
-            }
-        },
-        compass: {
-            options: {
-                require: [
-                    'breakpoint'
-                ],
-                cssDir: 'css',
-                sassDir: 'scss',
-                environment: 'development',
-                imagesDir: 'img',
-                javascriptsDir: 'js',
-                outputStyle: 'nested', //nested, expanded, compact, compressed
-                relativeAssets: true,
-            },
-            dev: {
-                options: {
-                    force: true
-                }
-            },
-            prod: {
-                options: {
-                    outputStyle: 'expanded',
-                    noLineComments: true
-
-                }
-            }
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      sass: {
+        files: [
+          'scss/**/*.scss'
+        ],
+        tasks: [
+          'compass:dev',
+          'beep:3'
+        ]
+      }
+    },
+    compass: {
+      options: {
+        require: [
+          'breakpoint'
+        ],
+        cssDir: 'css',
+        sassDir: 'scss',
+        environment: 'development',
+        imagesDir: 'img',
+        javascriptsDir: 'js',
+        outputStyle: 'nested', //nested, expanded, compact, compressed
+        relativeAssets: true,
+        force: true
+      },
+      dev: {
+        options: {
+          force: true
         }
-    });
+      },
+      prod: {
+        options: {
+          noLineComments: true
 
-    // Load the plugins
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-beep');
+        }
+      }
+    },
+    cssmin: {
+      all: {
+        files: [
+          {
+            src: 'css/cta.css',
+            dest: 'css/cta.min.css'
+          },
+          {
+            src: 'css/cta-flat.css',
+            dest: 'css/cta-flat.min.css'
+          },
+          {
+            src: 'css/site.css',
+            dest: 'css/site.min.css'
+          }
+        ]
+      }
+    }
+  });
 
-    // Development
-    grunt.registerTask('dev', 'Development build', function(args) {
-       // grunt.log.write("my message");
-        grunt.task.run([
-            'compass:dev',
-            'beep:3'
-        ]);
-    });
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-beep');
 
-    grunt.registerTask('prod', 'Production build', function(args) {
-        grunt.task.run([
-            'compass:prod',
-            'beep:3'
-        ]);
-    });
+  // Development
+  grunt.registerTask('dev', 'Development build', function(args) {
+     // grunt.log.write("my message");
+    grunt.task.run([
+      'compass:dev',
+      'beep:3'
+    ]);
+  });
 
-    // Default task (Force to development build)
-    grunt.registerTask('default', 'dev');
+  grunt.registerTask('prod', 'Production build', function(args) {
+    grunt.task.run([
+      'compass:prod',
+      'cssmin:all',
+      'beep:3'
+    ]);
+  });
+
+  // Default task (Force to development build)
+  grunt.registerTask('default', 'dev');
 };
